fc90602f4afcfa112ceff8a5fa352388adfad5c8
holger <h.trahe@relux.com>
2021-02-23 18:52:07 +0100

    [FIX] foxes dj3 py3 related

--- django_callable_perms/utils.py
+++ django_callable_perms/utils.py
@@ -1,10 +1,10 @@
 def autoload_permission_checks():
     from django.conf import settings
-    # Workaround for Django 1.8 
+    # Workaround for Django >=1.8
     try:
-        from django.utils.importlib import import_module
-    except ImportError:
         from importlib import import_module
+    except ImportError:
+        from django.utils.importlib import import_module
 
     from django.utils.module_loading import module_has_submodule
     
